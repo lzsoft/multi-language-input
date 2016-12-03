51e27c530c9e1d392a17070a859d03df84278c31
JingAn Chen <lzsoft.cja@gmail.com>
2016-12-03 16:35:27 -0500

    Add a "dejsonize" event for interaction.

--- multi.language.input.js
+++ multi.language.input.js
@@ -1,49 +1,55 @@
 (function () {
     'use strict';
     const AF_JSON = "data-json";
+    const EV_DEJSONIZE = "dejsonize";
     window.customElements.define('multi-language-input', class extends HTMLElement {
         constructor() {
             super();
             //
+            let self = this;
             let userInput = null;
-            this.innerHTML = document.querySelector('link[href*="multi.language.input.html"]').import.querySelector("template").innerHTML;
-            if (this.getAttribute("data-type") === "textarea") {
-                this.querySelector(":scope > input").remove();
-                this.querySelector(":scope > textarea").placeholder = this.title;
-                userInput = this.querySelector(":scope > textarea");
+            self.innerHTML = document.querySelector('link[href*="multi.language.input.html"]').import.querySelector("template").innerHTML;
+            if (self.getAttribute("data-type") === "textarea") {
+                self.querySelector(":scope > input").remove();
+                self.querySelector(":scope > textarea").placeholder = self.title;
+                userInput = self.querySelector(":scope > textarea");
             } else {
-                this.querySelector(":scope > textarea").remove();
-                this.querySelector(":scope > input").placeholder = this.title;
-                userInput = this.querySelector(":scope > input");
+                self.querySelector(":scope > textarea").remove();
+                self.querySelector(":scope > input").placeholder = self.title;
+                userInput = self.querySelector(":scope > input");
             }
-            this.querySelector(":scope > select").addEventListener("change", () => {
-                userInput.value = this.currentLanguageVal;
+            self.querySelector(":scope > select").addEventListener("change", () => {
+                userInput.value = self.currentLanguageVal;
             });
             userInput.addEventListener("blur", (e) => {
-                this.currentLanguageVal = userInput.value;
-                this.dispatchEvent(new Event("blur"));
+                self.currentLanguageVal = userInput.value;
+                self.dispatchEvent(new Event("blur"));
+            });
+            self.addEventListener(EV_DEJSONIZE, function () {
+                self.querySelector(":scope > select").dispatchEvent(new Event("change"));
             });
         }
         get currentLanguageVal() {
+            let self = this;
             let data = {};
             try {
-                data = JSON.parse(this.getAttribute(AF_JSON) || "{}") || {};
+                data = JSON.parse(self.getAttribute(AF_JSON) || "{}") || {};
             } catch (e) {
-                console.error("The string multi-language-input try to parse is not a valid JSON: " + this.getAttribute(AF_JSON));
+                console.error("The string multi-language-input try to parse is not a valid JSON: " + self.getAttribute(AF_JSON));
             }
-            let lang = this.querySelector(":scope > select").value;
+            let lang = self.querySelector(":scope > select").value;
             return data[lang] || "";
         }
         set currentLanguageVal(value) {
             let data = {};
             try {
-                data = JSON.parse(this.getAttribute(AF_JSON) || "{}") || {};
+                data = JSON.parse(self.getAttribute(AF_JSON) || "{}") || {};
             } catch (e) {
-                console.error("The string multi-language-input try to parse is not a valid JSON: " + this.getAttribute(AF_JSON));
+                console.error("The string multi-language-input try to parse is not a valid JSON: " + self.getAttribute(AF_JSON));
             }
-            let lang = this.querySelector(":scope > select").value;
+            let lang = self.querySelector(":scope > select").value;
             data[lang] = value;
-            this.setAttribute(AF_JSON, JSON.stringify(data));
+            self.setAttribute(AF_JSON, JSON.stringify(data));
         }
     });
 }());
